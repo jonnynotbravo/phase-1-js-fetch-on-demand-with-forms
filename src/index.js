4f914f5a3d83b494f82f74f7a6edc4d8d5ff1ba1
Jonny Tilahun <Jonnybiruk13@gmail.com>
2022-10-16 16:41:46 -0400

    commit tree 4fffc5aaf3723bfe31aed82ce219a7253be521cc

--- src/index.js
+++ src/index.js
@@ -1,5 +1,41 @@
 const init = () => {
+
+    const inputForm = document.querySelector('form');
+    const userInput = document.querySelector('#searchByID');
+    const title = document.createElement('li');
+    const summary = document.createElement('li');
+    const titleHeader = document.querySelector('h4');
+    const summaryHeader = document.querySelector('p');
+
   
+    inputForm.addEventListener('submit', (e) => {
+      e.preventDefault();
+    
+
+    
+      fetch(`http://localhost:3000/movies/${userInput.value}`)
+      .then(resp => resp.json())
+      .then(data => domManipulation(data));
+
+      
+      function domManipulation(data) {
+
+        title.innerText = data.title;
+        title.style.fontWeight = 'normal';
+        summary.innerText = data.summary;
+
+        titleHeader.appendChild(title);
+        summaryHeader.appendChild(summary);
+       
+
+        title.innerText = data.title;
+        summary.innerText = data.summary;
+
+        inputForm.reset();
+      }
+    
+
+    })
 }
 
 document.addEventListener('DOMContentLoaded', init);
